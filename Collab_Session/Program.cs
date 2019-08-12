4cecc13fb936ede7460c68dad0ac55e187defd74
RoyalKiwi <finnradloff@gmail.com>
2019-08-12 13:37:46 +1200

    comitt

--- Collab_Session/Program.cs
+++ Collab_Session/Program.cs
@@ -7,6 +7,9 @@ namespace Collab_Session
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Bye World!");
+
         }
     }
 }
